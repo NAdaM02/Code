007302eab3b57bb9410649b9e0a36ffbe3cb3317
NAdaM02 <adam.ancsin1@gmail.com>
2024-09-12 12:46:14 +0200

    09. 12. - 12:46

--- .__SYNC__.bat
+++ .__SYNC__.bat
@@ -2,22 +2,20 @@
 
 :: Add all files to the staging area
 
-git add *
+git add .
 
 for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
     set "COMMIT_MSG=%%a:%%b"
 )
 set "COMMIT_MSG=%DATE% - %COMMIT_MSG%"
 
-git commit * -m "%COMMIT_MSG%"
-
-git commit * --amend -m "%COMMIT_MSG%"
+git commit -m "%COMMIT_MSG%"
 
 echo Pulling changes from the remote repository...
-git pull --tags origin main
+git pull
 
 echo Pushing changes...
-git push --force
+git push
 
 echo.
 echo Upload successful.
