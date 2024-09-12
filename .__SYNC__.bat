94e9b8cfacd05bb6e3e65feb2aedd34692ef670d
NAdaM02 <adam.ancsin1@gmail.com>
2024-09-12 12:32:02 +0200

    Sync fixed

--- .__SYNC__.bat
+++ .__SYNC__.bat
@@ -1,6 +1,7 @@
 @echo off
 
 :: Add all files to the staging area
+
 git add *
 
 for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
@@ -8,9 +9,9 @@ for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
 )
 set "COMMIT_MSG=%DATE% - %COMMIT_MSG%"
 
-git commit -m "%COMMIT_MSG%"
+git commit * -m "%COMMIT_MSG%"
 
-git commit --amend -m "%COMMIT_MSG%"
+git commit * --amend -m "%COMMIT_MSG%"
 
 echo Pulling changes from the remote repository...
 git pull --tags origin main
