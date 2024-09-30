70de6582a4b886360a204bfe293f90223c79d547
NAdaM <adam.ancsin1@gmail.com>
2024-09-30 20:21:34 +0200

    09. 30. -

--- path_variables/push.bat
+++ path_variables/push.bat
@@ -6,31 +6,15 @@ for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set REPO_PATH=%%i
 
 cd %REPO_PATH%
 
-echo Syncing at %REPO_PATH%
+echo Pushing at %REPO_PATH%
 echo.
-
 
 git checkout main
 
-git add .
-
-for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
-    set "COMMIT_MSG=%%a:%%b"
-)
-set "COMMIT_MSG=%DATE% - %COMMIT_MSG%"
-
-git commit -m "%COMMIT_MSG%"
-
-echo.
-echo PULL-ing changes...
-git pull
-
-echo.
-echo PUSH-ing changes...
 git push
 
 echo.
-echo Syncing finished.
+echo Pushing finished.
 echo.
 
 cd %call_dir% > nul
