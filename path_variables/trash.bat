90410be6882b30b87afc2d197bd60716f8c9ab79
NAdaM <adam.ancsin1@gmail.com>
2024-09-30 20:38:06 +0200

    09. 30. - 20:38

--- path_variables/trash.bat
+++ path_variables/trash.bat
@@ -6,19 +6,17 @@ for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set REPO_PATH=%%i
 
 cd %REPO_PATH%
 
-echo Trashing and pulling %REPO_PATH%
+echo Trashing and Pulling %REPO_PATH%
 echo.
 
 git checkout -- .
-
-git fetch origin
 
 git reset --hard origin/main
 
 git clean -fd
 
 echo.
-echo Pulling finished.
+echo Trashing and Pulling finished.
 echo.
 
 cd %call_dir% > nul
