e1963de1e05498c30fba6a629274a58e2709ba04
NAdaM02 <adam.ancsin1@gmail.com>
2024-11-29 09:46:17 +0100

    11. 29. -  9:46

--- path_variables/commands/git/s..ps1
+++ path_variables/commands/git/s..ps1
@@ -1,5 +1,5 @@
-&scriptLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
-& "$PSScriptRoot/../../folders/coder.ps1"
+$scriptLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+& "$scriptLocation/../../folders/coder.ps1"
 & "sync.bat"
 & "$scriptLocation/../../folders/iskola.ps1"
 & "sync.bat"
