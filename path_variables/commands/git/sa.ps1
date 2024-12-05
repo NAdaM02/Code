2496a26f9727c7147b1539e0f851b4c3cb47fdd4
NAdaM02 <adam.ancsin1@gmail.com>
2024-12-05 10:46:59 +0100

    12. 05. - 10:46

--- path_variables/commands/git/sa.ps1
+++
@@ -1,5 +0,0 @@
-$scriptLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
-& "$scriptLocation/../../folders/coder.ps1"
-& "sync.bat"
-& "$scriptLocation/../../folders/iskola.ps1"
-& "sync.bat"
--- path_variables/commands/git/si.ps1
+++
@@ -1,3 +0,0 @@
-$scriptLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
-& "$scriptLocation/../../folders/iskola.ps1"
-& "sync.bat"
