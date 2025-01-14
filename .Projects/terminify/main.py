198f22136908aee80910ef5edf4dffbe67135b82
NAdaM02 <adam.ancsin1@gmail.com>
2025-01-14 15:00:19 +0100

    01. 14. - 15:00

--- .Projects/terminify/main.py
+++ .Projects/terminify/main.py
@@ -1,20 +1,31 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import sys
 
-sp = spotipy.Spotify(
+spotify = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id='67c0740055b9412da3e1e14978c42742',
         client_secret='4116025cf230497699d6feb972d8bfc7',
         redirect_uri='http://localhost',
-        scope="user-library-read",
-        )
+        ),
     )
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
 
-for album in albums:
-    print(album['name'])
+"""if len(sys.argv) > 1:
+    name = ' '.join(sys.argv[1:])
+else:
+    name = 'Radiohead'
+
+results = spotify.search(q='artist:' + name, type='artist')
+items = results['artists']['items']
+if len(items) > 0:
+    artist = items[0]
+    print(artist['name'], artist['images'][0]['url'])"""
+
+
+artist = spotify.artist(urn)
+print(artist)
+
+user = spotify.user('plamere')
+print(user)
