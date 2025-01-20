2286064ac77da1e591365592db1ca21c34785d0d
NAdaM02 <adam.ancsin1@gmail.com>
2025-01-20 23:52:54 +0100

    01. 20. - 23:52

--- .Projects/terminify/main.py
+++ .Projects/terminify/main.py
@@ -10,6 +10,26 @@ from sys import stdout
 from PIL import Image
 import asyncio
 import colorama
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import requests
+from io import BytesIO
+
+
+TIME_CONVERT_LIST = (29030400, 604800, 86400, 3600, 60, 1)
+TIME_CHAR_LIST = ('y', 'w', 'd', 'h', 'm', 's')
+
+def secs_to_text(seconds:float) :
+    seconds = int(seconds)
+    text = ""
+    for i in range(len(TIME_CONVERT_LIST)):
+        conv = TIME_CONVERT_LIST[i]
+        if conv <= seconds:
+            un = seconds//conv
+            seconds -= un*conv
+            text += str(un) + TIME_CHAR_LIST[i] + " "
+    text = f'{text}\b'
+    return text
 
 DOT = (os.path.dirname(__file__)).replace('\\','/')
 
@@ -18,6 +38,7 @@ OPAS = tuple(" .`':,_-;^!<+>=/*?|vLclTxY()r1iz{}tnsJjfCuo7FI][e3aVX2yZShk4AUPw5b
 THRESHOLDS = (19.92, 36.93, 39.75, 39.84, 42.89, 53.66, 54.4, 62.83, 69.53, 74.17, 105.29, 106.47, 108.4, 109.08, 111.08, 116.27, 117.73, 123.66, 125.45, 125.71, 128.68, 130.03, 131.86, 132.5, 135.4, 137.24, 137.5, 142.01, 142.56, 142.75, 143.7, 146.49, 146.71, 147.49, 148.18, 149.43, 149.48, 153.02, 153.36, 155.5, 156.51, 157.07, 157.39, 157.42, 157.54, 157.6, 157.9, 162.86, 163.27, 165.91, 166.07, 167.35, 167.53, 169.5, 170.98, 172.68, 174.85, 181.89, 182.69, 185.28, 185.88, 186.98, 188.07, 188.71, 194.11, 194.94, 195.06, 195.96, 196.18, 196.41, 196.81, 197.21, 197.27, 198.77, 200.79, 203.01, 204.88, 210.01, 212.57, 215.11, 218.92, 219.69, 221.34, 223.96, 232.33, 232.41, 232.83, 233.37, 240.17, 243.34, 255.0)
 
 CHAR_WIDTH_PER_HEIGHT = 78/155
+
 
 
 def print_separate(val_1, space_between:str, val_2):
@@ -130,19 +151,15 @@ class TerminalDisplay:
 
 
 class CustomImage:
-    def __init__(self, image_array=np.array([]), character:str=None):
+    def __init__(self, image_array=np.array([])):
         self.array = image_array
-        self.char = character
 
     def gray(self):
         self.array = cv2.cvtColor(np.array(self.array), cv2.COLOR_RGB2GRAY)
         return self
 
     def downscale(self, target_width:int, target_height:int):
-        if target_width == 1 and target_height == 1:
-            self.array = np.array(np.array(self.char))
-        else:
-            self.array = cv2.resize(self.array, (target_width, target_height), interpolation=cv2.INTER_AREA)
+        self.array = cv2.resize(self.array, (target_width, target_height), interpolation=cv2.INTER_AREA)
         return self
 
     def be_screenshot(self):
@@ -167,7 +184,22 @@ class CustomImage:
         return img_map
 
 
-ART_DICT = {
+
+
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id='67c0740055b9412da3e1e14978c42742',
+        client_secret='4116025cf230497699d6feb972d8bfc7',
+        redirect_uri='http://localhost:8080',
+        scope='user-read-playback-state user-modify-playback-state user-library-read user-library-modify'
+    )
+)
+
+
+
+
+ART_MAPS = {
     '0' : (".o.", "| |", "'0'"),
     '1' : (".~1", "  |", "  |"),
     '2' : ("˛=,", " ,I", "2__"),
@@ -186,49 +218,175 @@ ART_DICT = {
     'shuffle' :       ("~¸ ˛>", "  ¤  ", "~´ `>"),
     'smart_shuffle' : ("@¸ ˛>", "  ¤  ", "~´ `>"),
     'like' :  (",-.-,", "', ,'", "  `  "),
-    'liked' : (",=_=,", "\\"+"%"+"X%/", " ˇ÷ˇ ")
+    'liked' : (",=_=,", "\\"+"%"+"X%/", " ˇ÷ˇ "),
+    'cover_art' : ("o "*30 for _ in range(30)),
+    'progress_bar' : ("˙"*60, " "*60),
+    'next_up' : ("NEXT UP", "       "),
 }
-for key in ART_DICT.keys():
-    m = tuple(tuple(string) for string in ART_DICT[key])
-    ART_DICT[key] = np.array(m)
+for key in ART_MAPS.keys():
+    m = tuple(tuple(string) for string in ART_MAPS[key])
+    ART_MAPS[key] = np.array(m)
+
+ART_PLACES = {
+    'previous' : (31, 72),
+    'pause' :    (31, 77),
+    'resume' :   (31, 77),
+    'next' :     (31, 82),
+    'no_shuffle' :    (31, 65),
+    'shuffle' :       (31, 65),
+    'smart_shuffle' : (31, 65),
+    'like' :  (31, 87),
+    'liked' : (31, 87),
+    'cover_art' : (2,2),
+    'progress_bar' : (31,2),
+    'next_up' : (1,63),
+}
 
 
 
-"""import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-import sys
+def place_art(art_name):
+    row, col = ART_PLACES[art_name]
+    display_map.add_map_array(row, col, ART_MAPS[art_name])
 
-spotify = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id='67c0740055b9412da3e1e14978c42742',
-        client_secret='4116025cf230497699d6feb972d8bfc7',
-        redirect_uri='http://localhost'
-    )
-)"""
+def download_image(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    image = Image.open(BytesIO(response.content))
+    custom_image = CustomImage(np.array(image))
+    return custom_image
+
+def pad_with_spaces(array):
+    rows, cols = array.shape
+    
+    result = np.empty((rows, cols * 2 - 1), dtype=array.dtype)
+    
+    result[:, ::2] = array
+    result[:, 1::2] = ' ' 
+    
+    return result
+
+def update_album_cover(cover_map):
+    row, col = ART_PLACES['cover_art']
+    display_map.add_map_array(row, col, cover_map.array)
+
+
+
+
+
+current = None
+
+
+def toggle_shuffle():
+    if current:
+        shuffle_state = not current['shuffle_state']
+        sp.shuffle(state=shuffle_state)
+        return shuffle_state
+
+
+def stop_resume():
+    if current:
+        if current['is_playing']:
+            sp.pause_playback()
+            return 'Paused'
+        else:
+            sp.start_playback()
+            return 'Resumed'
+
+
+def next_track():
+    sp.next_track()
+    return 'Next track'
+
+
+def previous_track():
+    sp.previous_track()
+    return 'Previous track'
+
+
+def get_liked_status():
+    if current:
+        track_id = current['item']['id']
+        liked = sp.current_user_saved_tracks_contains([track_id])[0]
+        return liked
+
+
+def like_unlike_current_song():
+    if current:
+        track_id = current['item']['id']
+        liked = get_liked_status()
+        if liked:
+            sp.current_user_saved_tracks_delete([track_id])
+            return 'Unliked'
+        else:
+            sp.current_user_saved_tracks_add([track_id])
+            return 'Liked'
+
+def get_song_length():
+    if current:
+        song_length = round(current['item']['duration_ms'] / 1000)
+        return song_length
+
+
+def get_time():
+    if current:
+        current_time = round(current['progress_ms'] / 1000)
+        return current_time
+
+
+def get_next_queued_songs():
+    if current and 'queue' in current:
+        return [track['name'] for track in current['queue']]
+
+
+def get_album_cover_url():
+    if current:
+        return current['item']['album']['images'][0]['url']
+
+
+def update_current_album():
+    if current:
+        album_cover_url = get_album_cover_url()
+        album_cover = download_image(album_cover_url).to_map(60,30)
+        update_album_cover(album_cover)
+
+
+
+def update_progress_bar(progress):
+    progress = round(progress*60)
+    row, col = ART_PLACES['progress_bar']
+    display_map.add_map_array(row, col, np.array([tuple("~"*progress + "˝"*(60-progress))]))
 
 
 def song_view():
+    global current
     display_map.fill()
 
-    display_map.add_map_array(31, 65, ART_DICT['smart_shuffle'])
+    arts = ('smart_shuffle', 'previous', 'pause', 'next', 'liked', 'cover_art', 'progress_bar', 'next_up')
+    for art in arts: place_art(art)
 
-    display_map.add_map_array(31, 72, ART_DICT['previous'])
-    display_map.add_map_array(31, 77, ART_DICT['pause'])
-    display_map.add_map_array(31, 82, ART_DICT['next'])
+    #result = sp.search(q='sigma', limit=1, type='track')
 
-    display_map.add_map_array(31, 87, ART_DICT['liked'])
+    #track_cover_url = result['tracks']['items'][0]['album']['images'][0]['url']
 
-    terminal_display.update(display_map)
+    #terminal_display.update(display_map)
+    #print()
+    #print(track_cover_url)
+    while True:
+        current = sp.current_playback()
+        if current:
+            update_current_album()
 
-    """wait_seconds(2)
-    display_map.add_map_array(31, 65, ART_DICT['no_shuffle'])
-    terminal_display.update(display_map)
+            if get_liked_status():
+                place_art('liked')
+            else:
+                place_art('like')
 
-    wait_seconds(2)
-    display_map.add_map_array(31, 65, ART_DICT['shuffle'])
-    terminal_display.update(display_map)"""
+            song_length = get_song_length()
+            current_time = get_time()
+            update_progress_bar(current_time/song_length)
 
-
+            terminal_display.update(display_map)
 
 if __name__ == "__main__":
 
@@ -245,4 +403,4 @@ if __name__ == "__main__":
 
     song_view()
 
-    print(colorama.Style.RESET_ALL)
+    print(colorama.Style.RESET_ALL) # End terminal formatting
