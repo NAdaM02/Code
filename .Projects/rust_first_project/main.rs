47548048e2185ca95fd7dd747c224705e354b897
NAdaM <adam.ancsin1@gmail.com>
2025-05-13 12:48:04 +0200

    05. 13. - 12:47

--- .Projects/rust_first_project/main.rs
+++ .Projects/rust_first_project/main.rs
@@ -1,41 +1,10 @@
 fn main() {
-    let mut hh = 0;
-    let mut hv = 0;
-    let mut oh = 0;
+    // Try changing the values in the array, or make it a slice!
+    let array = [-1, 3, -2, 6];
 
-    let mut lh: u32;
-    let mut c: u32;
-    let mut o: u32;
-    let mut h: u32;
-    for n in 1..=703 {
-        lh = 0;
-        c = n;
-        o = 0;
-        println!("c: {:5}, lh: {:5}", c, lh);
-        h = loop {
-            if c != 1 {
-                if c % 2 == 0 {
-                    c = c/2;
-                } else {
-                    c = 3*c + 1;
-                }
-                if lh< c {
-                    lh = c;
-                    println!("c: {:5}, lh: {:5}", c, lh);
-                } else {
-                    println!("c: {:5}, lh:  ----", c)
-                }
-                o += 1;
-            } else {
-                break lh;
-            }
-        };
-        println!("\nHighest was: {h}\n\n\n\n");
-        if hh< h {
-            hh = h;
-            hv = n;
-            oh = o;
-        }
+    match array {
+        [-1, n @ .., 6 ] => println!("First is -1, last is 6, and inbetween is {n:?}"),
+        [.., 7] => println!("Last is 7, rest doesn't matter"),
+        _ => println!("Nothing special.")
     }
-    println!("hh: {}, hv: {}, oh: {}", hh, hv, oh);
 }
